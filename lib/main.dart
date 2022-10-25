20d94e6478628cfe0667977cb6d92771fd5f1ba8
MrSmooth07r <sandipstha2012@gmail.com>
2022-10-25 12:30:06 +0545

    khalti integration completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 import 'package:shapeup/screens/splashscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -13,14 +14,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        backgroundColor: Colors.white,
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
+    return KhaltiScope(
+      publicKey: 'test_public_key_47494fae63fd48158448b80bf362f95e',
+      builder: (context, navigatorKey) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          navigatorKey: navigatorKey,
+          supportedLocales: const [
+            Locale('en', 'US'),
+            Locale('ne', 'NP'),
+          ],
+          localizationsDelegates: const [
+            KhaltiLocalizations.delegate,
+          ],
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            backgroundColor: Colors.white,
+            primarySwatch: Colors.blue,
+          ),
+          home: const SplashScreen(),
+        );
+      },
     );
   }
 }
